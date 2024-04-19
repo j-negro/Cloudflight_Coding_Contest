2695ff0298a6712912caa179f0ea45ad2e914ab7
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2024-04-19 15:29:07 +0200

    Level 2 mbh

---
+++ src/level2/mbh.py
@@ -0,0 +1,25 @@
+for file in range(0, 6):
+    in_file = open(f"inputs/level1_{file}.in", "r")
+    out_file = open(f"outputs/mbh_level1_{file}.out", "w")
+
+    paths = int(in_file.readline())
+    for i in range(paths):
+        x, y = 0, 0
+        minx = maxx = maxy = miny = 0
+        path = in_file.readline()
+        for dir in path:
+            if dir == 'W':
+                y += 1
+            elif dir == 'A':
+                x -= 1
+            elif dir == 'S':
+                y += 1
+            else:
+                x += 1
+
+            minx = min(minx, x)
+            maxx = max(maxx, x)
+            miny = min(miny, y)
+            maxy = max(maxy, y)
+
+    out_file.write(f"{abs(minx - maxx)} {abs(miny-maxy)}\n")
